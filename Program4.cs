07b8062c40efd9dfe4922daae951339c7a5bcc5c
vinayaka-17 <75211643+vinayaka-17@users.noreply.github.com>
2021-04-12 12:07:12 +0530

    Add files via upload

---
+++ Program4.cs
@@ -0,0 +1,18 @@
+﻿
+    using System;
+    using System.Threading;
+
+    namespace MultithreadingApplication
+    {
+        class MainThreadProgram
+        {
+            static void Main(string[] args)
+            {
+                Thread th = Thread.CurrentThread;
+                th.Name = "MainThread";
+
+                Console.WriteLine("This is {0}", th.Name);
+                Console.ReadKey();
+            }
+        }
+    }
